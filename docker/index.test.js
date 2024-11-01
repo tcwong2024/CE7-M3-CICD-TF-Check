5e14d15a5abda190cea901fa8255a8424bbda9c6
tcwong2024 <tcwong99@hotmail.com>
2024-11-01 15:12:02 +0800

    Fixed index.test.js output must same as index.js

--- docker/index.test.js
+++ docker/index.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-  it('responds with "Hello, world!"', async () => {
+  it('responds with "Hello, TeckChoy!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, world!');
+    expect(response.text).toBe('Hello, TeckChoy!');
   });
 });
